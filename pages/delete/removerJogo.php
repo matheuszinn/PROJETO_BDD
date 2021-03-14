6664658c36ea0b8d9fc58af6344a88bd2eb47ec3
Matheus Alves <59948197+Matheuszinn@users.noreply.github.com>
2021-03-14 04:14:39 -0300

    erro grotesco de português

--- pages/delete/removerJogo.php
+++ pages/delete/removerJogo.php
@@ -144,7 +144,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                             if (count($results) == 1){
                                 echo ('Disponível na plataforma:');
                             }else{
-                                echo 'Disponível nas plataforma:';
+                                echo 'Disponíveis nas plataformas:';
                             }
 
                             foreach ($results as $plat){
